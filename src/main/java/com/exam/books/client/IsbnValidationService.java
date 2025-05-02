512dc190f305a9334f7a646dff21cdf5f5d868c3
Hugo Colin <colin.hugo.89@gmail.com>
2025-05-02 03:58:24 -0600

    Formato al codigo

--- src/main/java/com/exam/books/client/IsbnValidationService.java
+++ src/main/java/com/exam/books/client/IsbnValidationService.java
@@ -12,22 +12,28 @@ public class IsbnValidationService extends WebServiceGatewaySupport {
 
     private final WebServiceTemplate webServiceTemplate;
 
-    public boolean validateIsbn13(String isbn) {
-        IsValidISBN13 request = new IsValidISBN13();
-        request.setSISBN(isbn);
+    public boolean validateIsbn13( String isbn ) {
 
-        // Enviar la solicitud y obtener la respuesta
-        IsValidISBN13Response response = (IsValidISBN13Response) webServiceTemplate.marshalSendAndReceive("http://webservices.daehosting.com/services/isbnservice.wso", request);
+        IsValidISBN13 request = new IsValidISBN13();
+        request.setSISBN( isbn );
+
+        IsValidISBN13Response response = (IsValidISBN13Response) webServiceTemplate
+                .marshalSendAndReceive( "http://webservices.daehosting.com/services/isbnservice.wso", request );
+
         return response.isIsValidISBN13Result();
+        
     }
 
     public boolean validateIsbn10(String isbn) {
-        IsValidISBN10 request = new IsValidISBN10();
-        request.setSISBN(isbn);
 
-        // Enviar la solicitud y obtener la respuesta
-        IsValidISBN10Response response = (IsValidISBN10Response) webServiceTemplate.marshalSendAndReceive("http://webservices.daehosting.com/services/isbnservice.wso", request);
+        IsValidISBN10 request = new IsValidISBN10();
+        request.setSISBN( isbn );
+
+        IsValidISBN10Response response = ( IsValidISBN10Response ) webServiceTemplate
+                .marshalSendAndReceive( "http://webservices.daehosting.com/services/isbnservice.wso", request );
+
         return response.isIsValidISBN10Result();
+
     }
 
 }
--- src/main/java/com/exam/books/config/WebServiceConfig.java
+++ src/main/java/com/exam/books/config/WebServiceConfig.java
@@ -9,17 +9,23 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 public class WebServiceConfig {
 
     @Bean
-    public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
-        WebServiceTemplate webServiceTemplate = new WebServiceTemplate(marshaller);
-        webServiceTemplate.setDefaultUri("http://webservices.daehosting.com/services/isbnservice.wso");
+    public WebServiceTemplate webServiceTemplate( Jaxb2Marshaller marshaller ) {
+
+        WebServiceTemplate webServiceTemplate = new WebServiceTemplate( marshaller );
+        webServiceTemplate.setDefaultUri( "http://webservices.daehosting.com/services/isbnservice.wso" );
+
         return webServiceTemplate;
+
     }
 
     @Bean
     public Jaxb2Marshaller marshaller() {
+
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-        marshaller.setContextPath("com.exam.books.isbn"); // Paquete donde están las clases generadas
+        marshaller.setContextPath( "com.exam.books.isbn" );
+
         return marshaller;
+
     }
 
 }
